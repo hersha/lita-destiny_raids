f0abf46f0e098512a0d450c3d52e5fb99f4489f2
Patrick Neff <patrick.neff@me.com>
2016-03-11 21:22:13 -0800

    Test

--- lib/lita/handlers/destiny_raids.rb
+++ lib/lita/handlers/destiny_raids.rb
@@ -4,7 +4,9 @@ module Lita
       route(/^raid new (.*)$/, :new)
 
       def new(response)
-        name = response.inspect
+        name = response.user.metadata['mention_name'].nil? ?
+                 "#{response.user.name}" : 
+                 "#{response.user.metadata['mention_name']}"
         response.reply(name)
       end
 
